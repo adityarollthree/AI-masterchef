702b25cd170900a806745d89d10653fe9b3b161d
Aditya <rajaditya839@gmail.com>
2025-07-22 14:01:30 +0530

    Updated heading and title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header>
       <img src={chefLogo} alt="chef-claude-logo" />
-      <h1>Chef Claude</h1>
+      <h1>Chef on Clouds</h1>
     </header>
   );
 }
